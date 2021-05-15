2fe651ccd9758e4b935606d926276e5815bdfef0
Luis Angel Tormes Aguilera <luisangeltormes@gmail.com>
2021-05-14 17:24:16 -0700

    refactorizando el codigo

--- routes/routes.js
+++ routes/routes.js
@@ -1,87 +1,84 @@
 const express = require("express");
 const passport = require("passport");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const db = require('../settings/lowdb_config')
+const db = require("../settings/lowdb_config");
 const router = express.Router();
 
-router.get("/",(req,res,next) =>{
-
+router.get(
+  "/",
+  (req, res, next) => {
     const login = req.session.login;
 
-        if (login) {
-            console.log(req.session);
-            next();
-        } else {
-            res.render("index");
-            return;
-        }
-    },
-    (req, res) => {
-
-    res.render('./main')
-
-});
+    if (login) {
+      console.log(req.session);
+      next();
+    } else {
+      res.render("index");
+      return;
+    }
+  },
+  (req, res) => {
+    res.render("./main");
+  }
+);
 
 // login
 router.get("/login", (req, res) => {
-res.render("Login");
+  res.render("Login");
 });
 
 router.post(
   "/login",
   passport.authenticate("local", {
     failureRedirect: "/login",
-  }),(req, res)=>{                     
-        req.session.login = true;
-        res.redirect('./main')
-    }
+  }),
+  (req, res) => {
+    req.session.login = true;
+    res.redirect("./main");
+  }
 );
 // main----------------------------------------------------------
-router.get("/main",(req,res,next) =>{
-
+router.get(
+  "/main",
+  (req, res, next) => {
     const login = req.session.login;
 
-    if(!login){
-        res.status(401).send({ error: `Es nesesario estar Logueado!` });
-        return;
+    if (!login) {
+      res.status(401).send({ error: `Es nesesario estar Logueado!` });
+      return;
     }
     //console.log(req.session);
     next();
-
-}, (req, res) => {
-
+  },
+  (req, res) => {
     const user = req.session.passport.user;
-    res.render("main",{user});
-});
+    res.render("main", { user });
+  }
+);
 
 // cerra session
 
-router.get("/signout",(req,res)=>{
+router.get("/signout", (req, res) => {
   req.session.destroy();
-  res.redirect('/');
-})
+  res.redirect("/");
+});
 
 // register
-router.get('/register',(req,res)=>{
-    res.render('register');
-})
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-router.post('/register',(req,res)=>{
+router.post("/register", (req, res) => {
+  const { name, username, password } = req.body;
 
-const {name,username,password} = req.body
-
-bcrypt.genSalt(10,(err,salt)=>{
-bcrypt.hash(password,salt,(err,hash)=>{
-
-db.get('users').push({name,username,password:hash}).write()
-
-})
-})
-res.render('main',{user:{name}})
-
-
-})
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      db.get("users").push({ name, username, password: hash }).write();
+    });
+  });
+  res.render("main", { user: { name } });
+});
 
 //----------------------------------------------------------------------------------------------------
 
--- settings/lowdb_config.js
+++ settings/lowdb_config.js
@@ -1,14 +1,11 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
 // creando el adadtador y el dbObject
 
-const adapter = new FileSync('./database/db.json')
-const db = low(adapter)
+const adapter = new FileSync("./database/db.json");
+const db = low(adapter);
 
 // db.defaults({users:[]}).write()
 
 module.exports = db;
-
- 
-
